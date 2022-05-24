2a1a952335ea02d0c3378bc2a5613d079f2c2374
kerron <kerron68@gmail.com>
2022-05-24 17:34:09 +0100

    feat(mst): add hasData to uiStore

--- src/store/models/ui/uiStore.ts
+++ src/store/models/ui/uiStore.ts
@@ -3,15 +3,22 @@ import { Instance, types } from "mobx-state-tree";
 const UIStore = types
   .model("UIStore", {
     isDrawerOpen: false,
+    hasDataState: false,
   })
   .actions((self) => ({
     setDrawerState(val: boolean) {
       self.isDrawerOpen = val;
     },
+    setHasData(val: boolean) {
+      self.hasDataState = val;
+    },
   }))
   .views((self) => ({
     getDrawerState() {
       return self.isDrawerOpen;
+    },
+    get hasData(): boolean {
+      return self.hasDataState;
     },
   }));
 
